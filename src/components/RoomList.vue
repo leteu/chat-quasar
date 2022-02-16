3386661a33df1baee7c314364aa8e5e5073eba34
ghshin <leteu610@gmail.com>
2022-02-16 14:33:53 +0900

    방 리스트 레이아웃 변경

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,9 +1,10 @@
 <template>
   <q-list bordered separator class="full-height">
-    <template v-for="(item) in dataList" :key="`roomId-${item.id}`">
+    <template v-for="(item, index) in dataList" :key="`roomId-${item.id}`">
       <q-item class="flex items-center" clickable @click="evt => onClickRoom(evt, item)">
         {{item.name}}
       </q-item>
+      <q-separator v-if="dataList.length === (index+1)" />
     </template>
   </q-list>
 </template>
