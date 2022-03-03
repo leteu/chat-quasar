ea3cb8b069d092d860be636e100281c41caac733
ghshin <leteu610@gmail.com>
2022-03-03 14:29:20 +0900

    백엔드 url 수정

--- src/assets/common/config.ts
+++ src/assets/common/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://77c4-221-164-197-252.ngrok.io/';
-// const API_URL = 'http://172.16.10.103:8083/';
+const API_URL = 'https://bf5d-221-164-197-252.ngrok.io/';
+// const API_URL = 'http://172.16.10.103:8050/';
 axios.defaults.baseURL = API_URL;
 
 export default API_URL;
