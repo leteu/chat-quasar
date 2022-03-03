57e729339775656488d58d011a9cfee59e4a55ac
ghshin <leteu610@gmail.com>
2022-03-03 11:02:57 +0900

    [create] Auth Layout

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout class="bg-primary">
+    <router-view />
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'AuthLayout'
+})
+</script>
