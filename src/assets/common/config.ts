e0fff6edb11593875a681eb8770bdd294a15c213
ghshin <leteu610@gmail.com>
2022-03-03 10:58:28 +0900

    [update]backend url

--- src/assets/common/config.ts
+++ src/assets/common/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://948f-221-164-197-252.ngrok.io/';
+const API_URL = 'https://77c4-221-164-197-252.ngrok.io/';
 // const API_URL = 'http://172.16.10.103:8083/';
 axios.defaults.baseURL = API_URL;
 
