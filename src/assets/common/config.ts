822e40f9d64b26f6b3f8b6d0a2319fdc1483eaba
ghshin <leteu610@gmail.com>
2022-03-10 09:08:38 +0900

    백엔드 api 주소 변경

--- src/assets/common/config.ts
+++ src/assets/common/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://5e06-221-164-197-252.ngrok.io/';
+const API_URL = 'https://9b22-221-164-197-252.ngrok.io/';
 // const API_URL = 'http://172.16.10.103:8050/';
 axios.defaults.baseURL = API_URL;
 
